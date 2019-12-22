a7214ddf3da9fceb23d45426fed2fe6032b60864
Anton <blues.lead@gmail.com>
2019-12-22 19:03:20 +0200

    Started with boxplot

--- K_armed_bandit/test.py
+++ K_armed_bandit/test.py
@@ -8,13 +8,26 @@ Created on Thu Dec 12 18:37:17 2019
 from four_arm_class import KArmBandit
 import numpy as np
 from random import randrange
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+plt.style.use('seaborn-whitegrid')
 
 def main():
-    bandit = KArmBandit(4)
-    mus = np.array(([1,2,1,1]))
-    sigmas = np.array(([0.5,1.5,2.0,1.01]))
-    #test = bandit.generate_rewards(mus,sigmas)
-    mu_rewards = [0]*4
-    #IMPLEMENT select arm in the class
+    n_arms = 4
+    arms = []
+    df_arms = []
+    np.random.seed(None)
+    mus = np.random.normal(100, 50, 4)
+    sigmas = np.random.normal(20, 5, 4)
+    for i in range(n_arms):
+        arms.append(np.random.normal(mus[i], sigmas[i],1000))
+    classes = ['arm1','arm2','arm3','arm4']
+    for i in range(n_arms):
+        df_arms.append(pd.DataFrame({'score':arms[i], 'class':classes[i]}))
+    score_data = pd.concat(df_arms)
+    sns.boxplot(x='class', y='score', data=score_data)    
     
 main()
