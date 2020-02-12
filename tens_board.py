6e93806d925a8227e387f8006ba7d695b35d67b8
Anton <blues.lead@gmail.com>
2020-02-12 22:51:21 +0200

    some changes

--- tens_board.py
+++ tens_board.py
@@ -5,3 +5,13 @@ Created on Sun Jan  5 19:47:12 2020
 @author: Anton
 """
 
+import tensorflow as tf
+from datetime import datetime
+
+class GraphBoad:
+    def __init__(self, logdir):
+        now = datetime.now()
+        self.logdir = logdir
+        self.timestr = now.strftime("%Y%m%d-%H%M%S")
+    
+    def write_value()
