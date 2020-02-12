5701fec180b0dd8a62fb684e3ec3ee113eeb991e
kettu-metsanen <kettu.metsanen@gmail.com>
2020-02-12 23:08:37 +0200

    terveisia

--- tens_board.py
+++ tens_board.py
@@ -3,7 +3,7 @@
 Created on Sun Jan  5 19:47:12 2020
 for some reason empty file
 and one more comment for testing branching on github
-terveisia ketulle
+terveisia ketulle/ja minulle
 @author: Anton
 """
 
